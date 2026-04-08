94fa91b01549a0c2c38ae7ef0a772a3c9a6307be
Alex Mardle <alex.mardle@icloud.com>
2026-04-08 13:12:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-const isUserSite = repoName.endsWith('.github.io');
-const basePath = isGitHubActions && !isUserSite ? `/${repoName}` : '';
-
 const nextConfig = {
   output: 'export',
-  ...(basePath
-      ? {
-            basePath,
-            assetPrefix: `${basePath}/`
-        }
-      : {}),
 
   env: {
     stackbitPreview: process.env.STACKBIT_PREVIEW
@@ -22,9 +11,11 @@ const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
 
-  allowedDevOrigins: [
-    '192.168.1.84'
-  ]
+  allowedDevOrigins: ['192.168.1.84']
+};
+
+module.exports = nextConfig;
+
 };
 
 module.exports = nextConfig;
